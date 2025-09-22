7c8a00076c0fc4f3445162448ccf52d9a10c767c
dinoflask <76608290+dinoflask@users.noreply.github.com>
2025-09-22 17:27:20 -0400

    Set up with Maven and Game Class

---
+++ santorini/src/main/java/edu/Game.java
@@ -0,0 +1,23 @@
+package edu;
+
+import java.util.List;
+
+public class Game {
+    private List<Player> players;
+    private Board board;
+    private boolean winState;
+
+    public Game(List<Player> players, Board board) {
+        this.players = players;
+        this.board = board;
+        this.winState = false;
+    }
+
+    public void startGame() {
+        
+    }
+
+    public void nextTurn() {
+        
+    }
+}
---
+++ santorini/src/main/java/edu/cmu/cs214/santorini/Main.java
@@ -0,0 +1,10 @@
+package edu.cmu.cs214.santorini;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
